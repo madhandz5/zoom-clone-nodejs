4e8829d32368415dd00cb11a580876b1d2b98dfc
Ryan Oh <madhandz5@gmail.com>
2021-12-22 14:54:43 +0900

    feat : open socket server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,28 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message : ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("DisConnected to Server ❌");
+});
+
+function handleMessage(message) {
+  messageList.appendChild("li");
+  messageList.querySelector("li").innerText = message.data;
+}
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,8 +8,19 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("DisConnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("Socket Server Connected ✅");
+});
+
+server.listen(3000, () => console.log(`Listening on http://localhost:3000`));
